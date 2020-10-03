ba873433c2bb84b57ecdc0787304321cc92afaca
Andrej <andrej_miska_99@mail.ru>
2020-10-04 01:23:51 +0300

    Andrej Miskelovic SecondTask

---
+++ Caesar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+
+public class RecursiveFileProcessor
+{
+
+    class Program
+    {
+
+        public static string encrypt(string s, int k)
+        {
+            string encpass = "";
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (char)((arr[i] + k) % 128);
+                encpass = encpass + arr[i];
+            }
+            return encpass;
+        }
+
+        public static string decrypt(string s, int k)
+        {
+            string decpass = "";
+            char[] arr = s.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (char)((arr[i] + 128 - k % 128) % 128);
+                decpass = decpass + arr[i];
+            }
+            return decpass;
+        }
+ 
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type a string to encrypt: ");
+            string UserString = Console.ReadLine();
+
+            Console.WriteLine("Enter your Key: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Encrypted Data");
+            string cipherText = encrypt(UserString, key);
+            Console.WriteLine(cipherText);
+
+            Console.WriteLine("Decrypted Data: ");
+            string t = decrypt(cipherText, key);
+            Console.WriteLine(t);
+
+        }
+    }
+}
---
+++ Caesar.rb
@@ -0,0 +1,18 @@
+class Caesar
+    def encrypt(string, key)
+        string_to_ascii_array = string.chars.map {|char| char.ord}
+        shifted = string_to_ascii_array.map {|char| char+key}
+        shifted.map { |char| char.chr }.join
+        end
+
+    def decrypt(string, key)
+        string_to_ascii_array = string.chars.map {|char| char.ord}
+        shifted = string_to_ascii_array.map {|char| char-key}
+        shifted.map { |char| char.chr }.join
+        end
+
+end
+ 
+encrypt = Caesar.new.encrypt("Hello World", 5)
+puts(encrypt)
+puts(Caesar.new.decrypt(encrypt, 5))
